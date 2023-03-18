5e1b476e8a4e89ff5ac33d90cafba5017b579e22
danieljuma5 <jumadaniel275@gmail.com>
2023-03-18 10:12:09 +0300

    Completed assignment

--- src/02-advanced-debug.js
+++ src/02-advanced-debug.js
@@ -26,3 +26,158 @@ function goodPractices() {
 
 // then, call the function so it runs!
 goodPractices()
+
+function numPointsScored(playerName) {
+  // Loop through each team object
+  for (const team in gameObject) {
+    const players = gameObject[team]["players"];
+    // Loop through each player's stats
+    for (const player in players) {
+      if (player === playerName) {
+        // If the player's name matches the argument, return their points scored
+        return players[player]["points"];
+      }
+    }
+  }
+  // If not found, return undefined
+  return undefined;
+}
+
+
+function shoeSize(playerName) {
+  // Loop through each team's players
+  for (const team in gameObject) {
+    const players = gameObject[team]["players"];
+    // Loop through each player's stats
+    for (const player in players) {
+      if (player === playerName) {
+        // If the player's name matches the argument, return their shoe size
+        return players[player]["shoe"];
+      }
+    }
+  }
+  // If the player is not found, return undefined
+  return undefined;
+}
+
+function teamColors(teamName) {
+  // Loop through each team in gameObject
+  for (const team in gameObject) {
+    if (gameObject[team]["teamName"] === teamName) {
+      // If the team name matches the argument, return the team's colors
+      return gameObject[team]["colors"];
+    }
+  }
+  // If the team is not found, return undefined
+  return undefined;
+}
+
+
+function teamNames() {
+  // Create an array to hold the team names
+  const teamNamesArray = [];
+  // Loop through each team in gameObject
+  for (const team in gameObject) {
+    // Push the team name to the array
+    teamNamesArray.push(gameObject[team]["teamName"]);
+  }
+  return teamNamesArray;
+}
+
+function playerNumbers(teamName) {
+  // Loop through each team in gameObject
+  for (const team in gameObject) {
+    if (gameObject[team]["teamName"] === teamName) {
+      // If the team name matches the argument, create an array to hold the player numbers
+      const playerNumbersArray = [];
+      // Loop through each player on the team
+      for (const player in gameObject[team]["players"]) {
+        // Push the player's number to the array
+        playerNumbersArray.push(gameObject[team]["players"][player]["number"]);
+      }
+      return playerNumbersArray;
+    }
+  }
+  // If the team is not found, return undefined
+  return undefined;
+}
+
+function playerStats(playerName) {
+  // Loop through each team in gameObject
+  for (const team in gameObject) {
+    // Loop through each player on the team
+    for (const player in gameObject[team]["players"]) {
+      // If the player name matches the argument, return the player's stats
+      if (player === playerName) {
+        return gameObject[team]["players"][player];
+      }
+    }
+  }
+  // If the player is not found, return undefined
+  return undefined;
+}
+
+function bigShoeRebounds() {
+  let largestShoeSize = 0;
+  let playerWithLargestShoe = "";
+  // Loop through each team in gameObject
+  for (const team in gameObject) {
+    // Loop through each player on the team
+    for (const player in gameObject[team]["players"]) {
+      const currentShoeSize = gameObject[team]["players"][player]["shoe"];
+      // If the current player's shoe size is larger than the largest shoe size so far, update largestShoeSize and playerWithLargestShoe
+      if (currentShoeSize > largestShoeSize) {
+        largestShoeSize = currentShoeSize;
+        playerWithLargestShoe = player;
+      }
+    }
+  }
+  // Return the number of rebounds for the player with the largest shoe size
+  return gameObject[playerWithLargestShoe]["players"][playerWithLargestShoe]["rebounds"];
+}
+function mostPointsScored(game) {
+  let maxPoints = 0;
+  let playerName = "";
+  for (let team in game) {
+    for (let player in game[team].players) {
+      if (game[team].players[player].points > maxPoints) {
+        maxPoints = game[team].players[player].points;
+        playerName = player;
+      }
+    }
+  }
+  return playerName;
+}
+
+function winningTeam(game) {
+  let homePoints = 0;
+  let awayPoints = 0;
+  for (let player in game.home.players) {
+    homePoints += game.home.players[player].points;
+  }
+  for (let player in game.away.players) {
+    awayPoints += game.away.players[player].points;
+  }
+  if (homePoints > awayPoints) {
+    return game.home.teamName;
+  } else if (awayPoints > homePoints) {
+    return game.away.teamName;
+  } else {
+    return "Tie game!";
+  }
+}
+
+function playerWithLongestName(game) {
+  let maxLength = 0;
+  let playerName = "";
+  for (let team in game) {
+    for (let player in game[team].players) {
+      if (player.length > maxLength) {
+        maxLength = player.length;
+        playerName = player;
+      }
+    }
+  }
+  return playerName;
+}
+
